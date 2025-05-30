75ea2800575d96e515696566a4ae3d6e13d7f655
Omkar Yadav <omkary357@gmail.com>
2025-05-30 22:33:59 +0530

    Update urls.py

--- sportify/urls.py
+++ sportify/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('payment-success/', views.payment_success, name='payment_success'),
      path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('create-admin/', views.create_admin),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
